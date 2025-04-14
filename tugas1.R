53e79ee1e61f338228ece78a910a3acf1cc4a99a
foliumus <foliumus@gmail.com>
2025-04-14 10:48:28 +0800

    Add files via upload

---
+++ tugas1.R
@@ -0,0 +1,11 @@
+set.seed(123)
+n <- 1000
+lambda <- 3
+unif <- runif(n)
+inv <- -log(pmax(1 - unif)) / lambda
+exp <- rexp(n, rate = lambda)
+par(mfrow = c(1, 2))
+hist(inv, breaks = 30, probability = TRUE, col = "lightblue",main = "Inverse-Transform Method", xlab = "x")
+curve(dexp(x, rate = lambda), add = TRUE, col = "magenta", lwd = 2)
+hist(exp, breaks = 30, probability = TRUE, col = "lightgreen",main = "Fungsi rexp", xlab = "x")
+curve(dexp(x, rate = lambda), add = TRUE, col = "magenta", lwd = 2)
