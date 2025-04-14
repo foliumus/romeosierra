d0182819cf9934f815666e413d261da2437326dc
foliumus <foliumus@gmail.com>
2025-04-14 12:39:54 +0800

    Update 6.R

--- 6.R
+++ 6.R
@@ -1,77 +1,96 @@
-# Load library
-library(nortest)   # Untuk uji Anderson-Darling dan Lilliefors
-library(ggplot2)   # Untuk visualisasi
-
-# Set seed agar hasil dapat direproduksi
+library(nortest)
+library(ggplot2)
+# Set seed 
 set.seed(123)
 
-# Parameter Data 1
-mu1 <- 110           # Mean
-sigma1 <- 65         # Standard deviation
+# Parameter untuk Data 1
+mu1 <- 60 + 50  # Mean
+sigma1 <- 15 + 50  # Standard deviation
 
-# Parameter Data 2
-mu2 <- 100           # Mean
-sigma2 <- 60         # Standard deviation
+# Parameter untuk Data 2
+mu2 <- 50 + 50  # Mean
+sigma2 <- 10 + 50  # Standard deviation
 
 # Jumlah sampel
-n <- 1000
+n <- 1000 
 
-# Generate data dari distribusi normal
+# Generate data dengan distribusi normal
 data1 <- rnorm(n, mean = mu1, sd = sigma1)
 data2 <- rnorm(n, mean = mu2, sd = sigma2)
 
-# Ringkasan data
+# Summary
 summary(data1)
 summary(data2)
 
-# ------------------ Uji Normalitas ------------------
-
-# Kolmogorov-Smirnov (KS Test)
+data1 <- data.frame(data1)
+data2 <- data.frame(data2)
+# Normalitas
+# KS
 ks.test(data1, "pnorm", mean = mean(data1), sd = sd(data1))
-ks.test(data2, "pnorm", mean = mean(data2), sd = sd(data2))
+ks.test(data2,  "pnorm", mean = mean(data2), sd = sd(data2))
 
-# Shapiro-Wilk Test
+# SW
 shapiro.test(data1)
 shapiro.test(data2)
 
-# Anderson-Darling Test
+# AD
 ad.test(data1)
 ad.test(data2)
 
-# Lilliefors Test
+# Lillie
 lillie.test(data1)
-lillie.test(data2)
-
-# ------------------ Visualisasi ------------------
+lillie.test(data2)\
 
 # Histogram
+# Set layout untuk menampilkan 2 histogram berdampingan
+
+par(mfrow = c(1, 2))  
+
+# Histogram Data1
 hist(data1, col = "blue", breaks = 30, main = "Histogram Data1", 
      xlab = "Value", ylab = "Frequency")
+
+# Histogram Data2
 hist(data2, col = "red", breaks = 30, main = "Histogram Data2", 
      xlab = "Value", ylab = "Frequency")
 
-# QQ Plot
-par(mfrow = c(1, 2))  # Layout berdampingan
+# Reset layout
+par(mfrow = c(1, 1))
+
+# Set layout untuk menampilkan 2 QQ Plot berdampingan
+par(mfrow = c(1, 2))  
+
+# QQ Plot untuk Data1
 qqnorm(data1, main = "QQ Plot Data1", col = "blue")
-qqline(data1, col = "red", lwd = 2)
+qqline(data1, col = "red", lwd = 2)  # Tambahkan garis referensi
 
+# QQ Plot untuk Data2
 qqnorm(data2, main = "QQ Plot Data2", col = "blue")
-qqline(data2, col = "red", lwd = 2)
+qqline(data2, col = "red", lwd = 2)  # Tambahkan garis referensi
 
-par(mfrow = c(1, 1))  # Reset layout
+# Reset layout ke default
+par(mfrow = c(1, 1))
 
-# PP Plot Function
+# Fungsi untuk membuat PP Plot
 pp_plot <- function(data, title) {
-  probs <- (1:length(data)) / (length(data) + 1)  # Empirical CDF
-  theo_quantiles <- pnorm(sort(data), mean(data), sd(data))  # Theoretical CDF
-  plot(theo_quantiles, probs, main = title,
-       xlab = "Theoretical CDF", ylab = "Empirical CDF",
-       col = "blue", pch = 19)
-  abline(0, 1, col = "red", lwd = 2)
+  probs <- (1:length(data)) / (length(data) + 1)  # Probabilitas empiris
+  theo_quantiles <- pnorm(sort(data), mean(data), sd(data))  # Probabilitas teoretis
+  
+  plot(theo_quantiles, probs, main = title, xlab = "Theoretical CDF", 
+       ylab = "Empirical CDF", col = "blue", pch = 19)
+  abline(0, 1, col = "red", lwd = 2)  # Garis referensi
 }
 
-# PP Plot
-par(mfrow = c(1, 2))  # Layout berdampingan
+# Set layout untuk 2 plot berdampingan
+par(mfrow = c(1, 2))
+
+# PP Plot untuk Data1
 pp_plot(data1, "PP Plot Data1")
+
+# PP Plot untuk Data2
 pp_plot(data2, "PP Plot Data2")
-par(mfrow = c(1, 1))  # Reset layout
+
+# Reset layout ke default
+par(mfrow = c(1, 1))
+
+
